98f6c1398a2147c7f4663528b2bb0b9310ed47a0
Collins Kipruto <collinskipruto036@gmail.com>
2025-08-13 15:38:52 +0300

    submit

---
+++ character/src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import client from "@/apolloClient";
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return  <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>;
+}
